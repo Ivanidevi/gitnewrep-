6a9a2857dbd220123261fea183aa8b2b563395d7
Ivanidevi <109068977+Ivanidevi@users.noreply.github.com>
2026-04-19 01:36:46 +0530

    first commit

---
+++ fmain.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI (may change per region)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyFirstEC2"
+  }
+}
